25734e319d2ac30fa03ab745b39c55e568fd423f
Ali Abbas Chadhar <163995321+aliabbaschadhar@users.noreply.github.com>
2025-05-16 18:51:32 +0500

    Update page.tsx

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -5,6 +5,7 @@ export default async function Home() {
   return (
     <main>
       <h1 className="text-3xl font-bold underline">Hello world</h1>
+      <h1>How are you!</h1>
       <p className="text-lg">Welcome to the Next.js app!</p>
       {user?.username}
       {user?.password}
